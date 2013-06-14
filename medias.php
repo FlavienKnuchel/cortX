d8f224a283e48d7d3e18d5cdacb79e64a83247d9
Anthony Da Silva <anthony.dasilva@heig-vd.ch>
2013-06-14 17:22:02 +0200

    creation de la page media en html fixe

---
+++ medias.php
@@ -0,0 +1,46 @@
+<?php
+/*
+* media.php
+*
+* Author : Anthony
+* Date : 14.06.2013
+*
+* Description :
+*/
+
+include 'header.php';
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Medias</title>
+    </head>
+    
+    <div class="video_media">
+            <a href="#">
+                <img src="img/thumbs/video/video1.jpg" alt="Miniature video"/>
+            </a>
+    </div>
+    <div class="video_media">
+            <a href="#">
+                <img src="img/thumbs/video/video1.jpg" alt="Miniature video"/>
+            </a>
+    </div>
+    <div class="video_media">
+            <a href="#">
+                <img src="img/thumbs/video/video1.jpg" alt="Miniature video"/>
+            </a>
+    </div>
+    <div class="video_media">
+            <a href="#">
+                <img src="img/thumbs/video/video1.jpg" alt="Miniature video"/>
+            </a>
+    </div>
+    
+    
+    <?php
+    include 'userbar.php';
+    ?>
+</html>
