6e699845bf78ccefe3b700d6c272221c30232da2
flavien knuchel <knuchelf@gmail.com>
2013-06-14 17:44:20 +0200

    création de la page et du template team

---
+++ team.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+* team.php
+*
+* Author :
+* Date : 14.06.2013
+*
+* Description : page describing the different collaborators of the TEDx team
+*/
+include 'header.php';
+$smarty->display('team.tpl');
+include 'userbar.php';
+?>
---
+++ templates/team.tpl
@@ -0,0 +1,9 @@
+<!--
+team.tpl
+
+Author : Flavien Knuchel
+
+Date : 14.6.2013
+Description : template of the general team page
+
+->
