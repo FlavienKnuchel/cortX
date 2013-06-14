da269f337060f4ef7d066a3f283896842d5e9be4
flavien knuchel <knuchelf@gmail.com>
2013-06-14 11:11:31 +0200

    ajout du header smarty

--- home.php
+++ home.php
@@ -1,4 +1,13 @@
 <?php
 
+// put full path to Smarty.class.php
+require('C:\wamp\www\Smarty\libs\Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('./templates');
+$smarty->setCompileDir('./templates_c');
+$smarty->setCacheDir('./cache');
+$smarty->setConfigDir('./configs');
+
 
 ?>
