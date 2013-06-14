1dbcc036f162d02affe708a573c39e65ca430aaa
flavien knuchel <knuchelf@gmail.com>
2013-06-14 17:48:31 +0200

    création de la page et du template events

---
+++ events.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+* event.php
+*
+* Author :
+* Date : 14.06.2013
+*
+* Description : page describing the actual event and the old events
+*/
+include 'header.php';
+$smarty->display('events.tpl');
+include 'userbar.php';
+?>
---
+++ templates/events.tpl
@@ -0,0 +1,9 @@
+<!--
+events.tpl
+
+Author : Flavien Knuchel
+
+Date : 14.6.2013
+Description : template of the events page
+
+->
