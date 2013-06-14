a3bef5233292823db744ec648bee19ab3389065a
Chris R <christophe.rast@heig-vd.ch>
2013-06-14 16:25:58 +0200

    clean commit fail chris home.php

--- home.php
+++ home.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /*
  * home.php
@@ -14,30 +13,8 @@
 include 'header.php';
 $smarty->display('home.tpl');
 include 'userbar.php';
-<<<<<<< HEAD
 $smarty->assign('NextEventName', 'getMainTopic()');
 $smarty->assign('NextEventDesc', 'getDescription()');
 
 
 ?>
-=======
-<?php
-/*
- * home.php
- *
- * Author : Flavien Knuchel
- * Date : 14.06.2013
- *
- * Description : home page that calls the header.php and userbar.php
- *
- *
- */
- 
-include 'header.php';
-$smarty->display('home.tpl');
-include 'userbar.php';
-=======
->>>>>>> 78863a779e2558050366fd9a023150a6edc6390b
-
-?>
->>>>>>> 78863a779e2558050366fd9a023150a6edc6390b
