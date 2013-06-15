a0036e2bf5de8c48051b15ecd12b33bf35f4fe18
Chris R <christophe.rast@heig-vd.ch>
2013-06-15 13:04:16 +0200

    Deplacement about.php -> about.tpl
    La structure html était dans le php au lieu d'être dans le tpl

--- about.php
+++ about.php
@@ -1,41 +1,15 @@
 <?php
+
 /*
-* about.php
-*
-* Author : Anthony
-* Date : 14.06.2013
-*
-* Description :
-*/
+ * about.php
+ *
+ * Author : Anthony
+ * Date : 14.06.2013
+ *
+ * Description :
+ */
 
 include 'header.php';
-
+$smarty->display('about.tpl');
+include 'userbar.php';
 ?>
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>About</title>
-    </head>
-    
-    
-    	<ul>
-            <li>
-                <a href="about_TED.php" title="TED">TED</a>
-            </li>
-            <li>
-                <a href="about_TEDx.php" title="TEDx">TEDx</a>
-            </li>
-            <li>
-                <a href="about_TEDxLausanne.php" title="TEDxLausanne">TEDxLausanne</a>
-            </li>
-            <li>
-                <a href="about_TEDxChange.php" title="TEDxChange">TEDxChange</a>
-            </li>
-    	</ul>
-    
-    
-    <?php
-    include 'userbar.php';
-    ?>
-</html>
---
+++ templates/about.tpl
@@ -0,0 +1,14 @@
+<ul>
+    <li>
+        <a href="about_TED.php" title="TED">TED</a>
+    </li>
+    <li>
+        <a href="about_TEDx.php" title="TEDx">TEDx</a>
+    </li>
+    <li>
+        <a href="about_TEDxLausanne.php" title="TEDxLausanne">TEDxLausanne</a>
+    </li>
+    <li>
+        <a href="about_TEDxChange.php" title="TEDxChange">TEDxChange</a>
+    </li>
+</ul>
