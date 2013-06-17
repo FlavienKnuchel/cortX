9d2538e85909ede5c849111d0498c64f199e3ac5
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-17 11:40:32 +0200

    Page speaker profil tpl done + default picture

---
+++ speaker_profil.php
@@ -0,0 +1,16 @@
+
+<?php
+/*
+* speakers.php
+*
+* Author : Andréane et Noémie
+* Date : 17.06.2013
+*
+* Description : profil of the speaker who participated in TEDx
+*/
+include 'header.php';
+$smarty->assign('speaker_name', 'getSpeaker Name');
+$smarty->assign('speaker_description', 'getSpeaker Description');
+$smarty->display('speaker_profil.tpl');
+include 'userbar.php';
+?>
