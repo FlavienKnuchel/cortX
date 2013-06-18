d632bbf12d62f324148daa98d29b55225c0ba1d9
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-18 14:29:56 +0200

    update backend_add_event php and done backend_update_event

--- backend_add_event.php
+++ backend_add_event.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
 * backend_add_event.php
---
+++ backend_update_event.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* backend_update_event.php
+*
+* Author : Andreane et Noemie
+* Date : 18.06.2013
+*
+* Description : page to create a new event, with slot and speaker
+*/
+include 'header.php';
+$smarty->assign('main_topic_event','getMainTopic Event');
+$smarty->assign('description_event','getDescription Event');
+$smarty->assign('location_name','getName Location');
+$smarty->assign('location_address','getAddress Location');
+$smarty->assign('location_town','getTown Location');
+$smarty->assign('location_country','getCountry Location');
+$smarty->display('backend_update_event.tpl');
+include 'userbar.php';
+?>
