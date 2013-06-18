9316d79c6e57600136abaee6050bcb1531ca08f8
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-18 10:02:09 +0200

    creaze

---
+++ backend_home.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+* backend_home.php
+*
+* Author : Andréane et Noémie
+* Date : 18.06.2013
+*
+* Description : backend with the links
+*/
+include 'header.php';
+$smarty->display('backend_home.tpl');
+include 'userbar.php';
+?>
