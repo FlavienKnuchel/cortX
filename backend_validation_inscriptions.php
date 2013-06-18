3512035f748a1c06a2f48ef78645d0d3e710f9c8
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-18 10:43:05 +0200

    Backend_validation_inscirptions and Backend_speakers done php

---
+++ backend_validation_inscriptions.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* backend_validation_inscriptions.php
+*
+* Author : Andreane et Noemie
+* Date : 18.06.2013
+*
+* Description : page to manage the inscriptions
+*/
+include 'header.php';
+$smarty->assign('participant_name','getName Participant');
+$smarty->display('backend_validation_inscriptions.tpl');
+include 'userbar.php';
+?>
