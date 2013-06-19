91208f79f0c62047d3cc137382a9cfafe9c5dc55
Chris R <christophe.rast@heig-vd.ch>
2013-06-19 16:47:03 +0200

    user inscription NOT STABLE DEV ONGOING

--- user_inscriptions.php
+++ user_inscriptions.php
@@ -11,41 +11,63 @@
  */
 include 'header.php';
 if ($tedx_manager->isLogged()) { //test if user is logged otherwise he displays some static text.
-    $person = $tedx_manager->getLoggedPerson()->getContent();
-    $arrayKeywords = $tedx_manager->getKeyWordsByPerson($person)->getContent();
-    for ($index = 0; $index < 3; $index++) {
-        if (!empty($arrayKeywords[$index])) {
-            $smarty->assign('kw' . ($index + 1), $arrayKeywords[$index]->getValue());
+//  Test the status of the registration
+    $person = $tedx_manager->getLoggedPerson()->getContent(); //retrieve the loged person
+    $event = $tedx_manager->getParticipation->getContent()->getEvent; //TODO with new AS
+//retrieve the status of current registration
+    $regStatus = $tedx_manager->getRegistration($event->getNo())->getContent()->getStatus();
+    if ($regStatus == 'submitted' || $regStatus == 'validated' || $regStatus == 'refused' || $regStatus == 'pending') {
+//    The content has been saved and submitted for evaluation
+        // the content is grayed and cannot be modified again
+    } else {
+        //the content is saved but not submitted yet and can be modified
+        for ($n = 0; $n < 3; $n++) {//cycle through the 3 keywords and modifies them if necessary
+            //if the keyword is not posted it is retrieved from the DB and displayed on the page
+            if (empty($_POST['Keyword' . ($n + 1)])) {
+                //retrieve the array of keywords
+                $args = array($person, $event);
+                $arrayKeywords = $tedx_manager->getKeyWordsByPersonForEvent($args)->getContent();
+                if (!empty($arrayKeywords[$n])) {//gets the nth KW
+                    $smarty->assign('kw' . ($n + 1), $arrayKeywords[$n]->getValue());
+                } else {
+                    $smarty->assign('kw' . ($n + 1), '');
+                }
+            } else {//if the keyword is posted, it is added in the DB
+                $args = array($arrayKeywords[$n], $person, $event); //retrieve previous KW from DB 
+                $tedx_manager->archiveKeyword($args)->getStatus(); //archive the previous KW
+                $kw = $_POST['Keyword' . ($n + 1)];
+                $args = array($kw, $person, $event);
+                $tedx_manager->addKeywordsToAnEvent($args); //add the new KW in place of the previous one
+            }
+        }//end FOR
+
+// ---- Attendre services des IT, changer motivation toussa, comme pour keyword
+        // retrieve the motivation
+        $oldMotivation = $tedx_manager->getMotivationByPersonForEvent($args)->getContent();
+        if (!empty($oldMotivation->getText())) {
+            $smarty->assign('motivation', $oldMotivation->getText());
         } else {
-            $smarty->assign('kw' . ($index + 1), 'Empty');
+            $smarty->assign('motivation', 'Empty');
         }
-    }
-//        $smarty->assign('kw2', $arrayKeywords[1]->getValue());
-//        $smarty->assign('kw3', $arrayKeywords[2]->getValue());
-//        $smarty->assign('kw1', 'pomme');
-//        $smarty->assign('kw2', 'peche');
-//        $smarty->assign('kw3', 'poire');
-// ---- Attendre services des IT 
-//      $oldMotivation = $person->getMotivation();
 //    $motivation = $_POST['motivation'];
-//    $kw1 = $_POST['Keyword1'];
-//    $kw2 = $_POST['Keyword2'];
-//    $kw3 = $_POST['Keyword3'];
-//    
+
+        
+        
 // ---- Get events and last events for the historic ---------------------------
 // declare the request arguments for events
-    $searchArgs = array();
+        $searchArgs = array();
 //get back the result of the events request
-    $messageSearchEvents = $tedx_manager->searchEvents($searchArgs);
+        $messageSearchEvents = $tedx_manager->searchEvents($searchArgs);
 //if the message is not an error message
-    if ($messageSearchEvents->getStatus()) {
-        //get back the list of all the events
-        $events = $messageSearchEvents->getContent();
-        //stock the events list in smarty
-        $smarty->assign('eventsObjects', $events);
-    }//if
+        if ($messageSearchEvents->getStatus()) {
+            //get back the list of all the events
+            $events = $messageSearchEvents->getContent();
+            //stock the events list in smarty
+            $smarty->assign('eventsObjects', $events);
+        }//if
+    }// end IF status
     $smarty->display('user_inscriptions.tpl');
-} else {
+} else {//---- print error message --------------------------------------------
     print ('<p class="error_msg">');
     print ("You are not logged in, you can't access this content.</p>");
 }
