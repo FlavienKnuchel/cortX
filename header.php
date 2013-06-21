431061328495ef6cd0c2eeaff73f1cf6b0392f4f
Chris R <christophe.rast@heig-vd.ch>
2013-06-21 09:50:17 +0200

    userbar.php test userLevel

--- header.php
+++ header.php
@@ -65,23 +65,23 @@ require_once('../tedx-config.php');
 
 $smarty->assign('loggedin', $tedx_manager->isLogged()); //assign value for smarty test
 
-/*
+//test the user level 
 if ($tedx_manager->isLogged()) {
-    if ($tedx_manager->isOrganizer()) {
-        $smarty->assign('userLevel', 'organizer');
-    }
     if ($tedx_manager->isParticipant()) {
         $smarty->assign('userLevel', 'participant');
     }
-    if ($tedx_manager->isAdministrator()) {
-        $smarty->assign('userLevel', 'administrator');
+    if ($tedx_manager->isOrganizer()) {
+        $smarty->assign('userLevel', 'organizer');
     }
     if ($tedx_manager->isValidator()) {
         $smarty->assign('userLevel', 'validator');
     }
+    if ($tedx_manager->isAdministrator()) {
+        $smarty->assign('userLevel', 'administrator');
+    }
     if ($tedx_manager->isSuperadmin()) {
         $smarty->assign('userLevel', 'superadmin');
     }
-}*/
+}
 ?>
 
