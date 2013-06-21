4c4e59cab30dca8bf61b7dc26a1dd495ee68b686
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-21 13:07:30 +0200

    update media style

--- medias.php
+++ medias.php
@@ -15,35 +15,39 @@ include 'menu_frontend.php';
     <div id="videos_media">        
         <h1>TEDxLausanne 2013</h1>
         <div class="row">
-            <a href="#" class="offset2 span2">
+            <div class="offset2 span8" >
+            <a href="#" class="">
                 <img src="img/videos/video1.jpg" alt="Miniature video"/>
             </a>
-           <a href="#" class="span2">
+           <a href="#" class="">
                 <img src="img/videos/video1.jpg" alt="Miniature video"/>
             </a>
-            <a href="#" class="span2">
+            <a href="#" class=" ">
                 <img src="img/videos/video1.jpg" alt="Miniature video"/>
             </a>
-            <a href="#" class="span2">
+            <a href="#" class="">
                 <img src="img/videos/video1.jpg" alt="Miniature video"/>
             </a>    
+                </div>
         </div>
     
         <div class="row">
             <h1>TEDxLausanne 2012</h1>
                 <div class="row">
-                <a href="#" class="offset2 span2">
+                    <div class="offset2 span8">
+                <a href="#" class="">
                     <img src="img/videos/video1.jpg" alt="Miniature video"/>
                 </a>
-               <a href="#" class="span2">
+               <a href="#" class="">
                     <img src="img/videos/video1.jpg" alt="Miniature video"/>
                 </a>
-                <a href="#" class="span2">
+                <a href="#" class="">
                     <img src="img/videos/video1.jpg" alt="Miniature video"/>
                 </a>
-                <a href="#" class="span2">
+                <a href="#" class="">
                     <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>    
+                </a>   
+                        </div>
             </div>
         </div>
     </div>
