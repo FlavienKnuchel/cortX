5818c7541ab1b10841f260cdb02632c9b15c6317
noemie sandoz <noemie.sandoz@gmail.com>
2013-06-22 16:03:58 +0200

    404 php

---
+++ 404.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * 404.php
+ *
+ * Author : noemie sandoz
+ * Date : 22.06.2013
+ *
+ * Description : page for 404 error
+ *
+ *
+ */
+ 
+?>
+<head>
+    <meta charset="utf-8" />
+    <LINK rel="stylesheet" media="all" href="css/cortx.css" />
+    <LINK rel="stylesheet" media="all" href="css/normalize.css" />
+    <LINK rel="stylesheet" media="all" href="css/grid.css" />
+    <title>CORTx</title>
+        <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+</head>
+<body>
+
+<section>
+	<article class="offset2 span8">
+        <h1 id="404">error 4<span class="x">x</span>4</h1>
+        
+            <p> We can't find the page you're looking for. Go back to <a href="#" title="Home">Home</a>.</p>
+    
+    </article>
+</section>
+    </body>
