3d4f8757fea6c6714c41a238fbc285ca16183413
Chris R <christophe.rast@heig-vd.ch>
2013-06-24 13:52:45 +0200

    video wall WUT ?!

--- medias.php
+++ medias.php
@@ -1,57 +1,17 @@
 <?php
+
 /*
-* media.php
-*
-* Author : Anthony Da Silva
-* Date : 14.06.2013
-*
-* Description :
-*/
+ * media.php
+ *
+ * Author : Anthony Da Silva
+ * Date : 14.06.2013
+ *
+ * Description : manages the video fo the events
+ */
 
 include 'header.php';
 include 'menu_frontend.php';
 
-?>
-    <div id="videos_media">        
-        <h1>TEDxLausanne 2013</h1>
-        <div class="row">
-            <div class="offset2 span8" >
-            <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>
-           <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>
-            <a href="#" class=" ">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>
-            <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>    
-                </div>
-        </div>
-    
-        <div class="row">
-            <h1>TEDxLausanne 2012</h1>
-                <div class="row">
-                    <div class="offset2 span8">
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-               <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>   
-                        </div>
-            </div>
-        </div>
-    </div>
 
-    <?php
-    include 'userbar.php';
-    ?>
+include 'userbar.php';
+?>
