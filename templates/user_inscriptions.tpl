37636c2bec8a19f72b8d629803f21fdd3a28f79e
Chris R <christophe.rast@heig-vd.ch>
2013-06-25 14:07:41 +0200

    user_inscriptions 1/2 formulaire pas encore fait

--- templates/user_inscriptions.tpl
+++ templates/user_inscriptions.tpl
@@ -11,90 +11,122 @@ Description : template of the user inscription
 <section id="inscription">
     {if $loggedin}
         {if $participant->getStatus()}
-            {if isset($registration) }
-                <article class="form_inscr">
-                    {if $regStatus}
-                        {section name=eventTopic loop=$eventsObjects}
-                            {if $smarty.section.eventTopic.last}
-                                <h2>You are applying to {$eventsObjects[eventTopic]->getMainTopic()}</h2>
-                            {/if}
-                        {/section}
-                        <h3>Your motivation</h3>
-                        <form method="POST" action="user_inscriptions.php">
-                            <label for="type">Your activity domain</label>
-                            <input type="text" name="type" placeholder="Your activity domain" required
-                                   value='{$registration->getType()}'>
-                            <label for="typeDescription">Your role in this domain</label>
-                            <input type="text" name="typeDescription" placeholder="Your role in this domain" required
-                                   value='{$registration->getTypeDescription()}'>
-                        </article>
-                            <label for="motivation" class="subtitle">Explain us your motivations</label>
-                            <textarea name="motivation" title="motivation" class="big" autofocus spellcheck>{$motivation}</textarea>
-                            <label for="Keyword" title="Describe youself in 3 words">Keywords describing you</label>
-                            <input type="text" name="Keyword1" value="{$kw1}" value="">
-                            <input type="text" name="Keyword2" value="{$kw2}" value="">
-                            <input type="text" name="Keyword3" value="{$kw3}" value="">
+            {if !empty($arrayReg) }
+                {section name=loopReg loop=$arrayReg}
+                    {if $arrayReg[loopReg]['registration']->getStatus() == 'Pending'}
+                        <article class="form_inscr">
 
-                            <!--<input type="submit" name="preview" value="preview of the badge" alt="preview of the badge">-->
-                            <input type="submit" name="Save" value="Save" alt="Save and edit later">
-                            <input type="submit" name="Send" value="Send" alt="Submit your registration request">
-                        </form>
-                    {else}
-                        {section name=eventTopic loop=$eventsObjects}
-                            {if $smarty.section.eventTopic.last}
-                                <h2>You applied to {$eventsObjects[eventTopic]->getMainTopic()}</h2>
-                            {/if}
-                        {/section}
-                        <p class="subtitle">You can view your appliance but cannot update it anymore.</p>
-                        <h3>Your motivation</h3>
-                        <p class="form_closed motivation">{$getMotivation}</p>
-                        <p class="form_closed keyword">{$getKW1}</p>
-                        <p class="form_closed keyword">{$getKW2}</p>
-                        <p class="form_closed keyword">{$getKW3}</p>
+                            <h2>You are applying to {$arrayReg[loopReg]['event']->getMainTopic()}</h2>
+
+                            <form method="POST" action="?id={$smarty.section.loopReg.index}">
+                                <label for="type">Your activity domain</label>
+                                <input type="text" name="type" placeholder="Your activity domain" required
+                                       value='{$arrayReg[loopReg]['registration']->getType()}'>
+                                <label for="typeDescription">Your role in this domain</label>
+                                <input type="text" name="typeDescription"  required
+                                       value="{$arrayReg[loopReg]['registration']->getTypeDescription()}">
+                                <label for="motivation" class="subtitle">Explain us your motivations</label>
+                                <textarea name="motivation" title="motivation" class="big" spellcheck>
+                                    {if isset($arrayReg[loopReg]['motivation'])}
+                                        {$arrayReg[loopReg]['motivation']->getText()}
+                                    {/if}</textarea>
+                                <label for="Keyword" title="Describe youself in 3 words">Keywords that describes you the best</label>
+                                <input type="text" name="Keyword1" value="
+                                       {if isset($arrayReg[loopReg]['arrayKW'][0])}
+                                           {$arrayReg[loopReg]['arrayKW'][0]->getValue()}
+                                       {/if}" >
+                                <input type="text" name="Keyword2" value="
+                                       {if isset($arrayReg[loopReg]['arrayKW'][1])}
+                                           {$arrayReg[loopReg]['arrayKW'][1]->getValue()}
+                                       {/if}" >
+                                <input type="text" name="Keyword3" value="
+                                       {if isset($arrayReg[loopReg]['arrayKW'][2])}
+                                           {$arrayReg[loopReg]['arrayKW'][2]->getValue()}
+                                       {/if}" >
+
+                                <!--<input type="submit" name="preview" value="preview of the badge" alt="preview of the badge">-->
+                                <input type="submit" name="Save" value="Save" alt="Save and edit later">
+                                <input type="submit" name="Send" value="Send" alt="Submit your registration request">
+                            </form>
+                        </article>
+                    {elseif ($arrayReg[loopReg]['registration']->getStatus() == 'Sent')}
+                        <article class="form_closed">
+                            <p class="subtitle">You applied to</p> 
+                            <h2>{$arrayReg[loopReg]['event']->getMainTopic()}</h2>
+                            <p class="subtitle">You can view your appliance but cannot update it anymore.</p>
+                            <p class="label" >Your activity domain:</p>
+                            <p class="activitydomain">{$arrayReg[loopReg]['registration']->getType()}</p>
+                            <p class="label">Your role in this domain</p>
+                            <p class="roledomain">{$arrayReg[loopReg]['registration']->getTypeDescription()}</p>
+                            <p class="label">Motivation:</p>
+                            <p class="motivation">{$arrayReg[loopReg]['motivation']->getText()}</p>
+                            <p class="label">Keywords:</p>
+                            <p class="keyword">{$arrayReg[loopReg]['arrayKW'][0]->getValue()}</p>
+                            <p class="keyword">{$arrayReg[loopReg]['arrayKW'][1]->getValue()}</p>
+                            <p class="keyword">{$arrayReg[loopReg]['arrayKW'][2]->getValue()}</p>
+                        </article>
 
                     {/if}
-                </article>
+                {/section}
             {else}
-                    <section id="row">
-                        <article class="offset2 span8 badMessage">
-                            <p>You Don't have any registrations yet</p>
-                        </article>
-                    </section>
+                <section id="row">
+                    <article class="offset2 span8 badMessage">
+                        <p>You Don't have any pending or sent registrations.</p>
+                    </article>
+                </section>
             {/if}
-            <aside id="oldevents">
-                <h2>Previous events</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <td>Topic</td>
-                            <td>Date</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {if isset($eventsObjects)}
-                            {section loop=$eventsObjects name=oldEvents step=-1}
-                            {if $smarty.section.oldEvents.first} {else}
-                                <tr>
-                                    <td>{$eventsObjects[oldEvents]->getMainTopic()}</td>
-                                    <td>{$eventsObjects[oldEvents]->getStartingDate()}</td>
-                                </tr>
-                            {/if}
-                        {/section}
-                    {else}
-                        <tr>
-                            <td>No previous events</td>
-                            <td></td>
-                        </tr>
-                    {/if}
-                </tbody>
-            </table>
-        </aside>
+            {if !empty($arrayOldReg) }
+                <aside id="oldevents">
+                    <h2>Previous registrations</h2>
+                    {section name=loopOldReg loop=$arrayOldReg}
+                        <article class="form_closed">
+                            <h3>{$arrayOldReg[loopOldReg]['event']->getMainTopic()}</h3>
+                            <p class="subtitle">You can view your appliance but cannot update it anymore.</p>
+                            <p class="label" >Your activity domain:</p>
+                            <p class="activitydomain">
+                                {if isset($arrayOldReg[loopOldReg]['oldReg'])}
+                                    {$arrayOldReg[loopOldReg]['oldReg']->getType()}
+                                {/if}
+                            </p>
+                            <p class="label">Your role in this domain</p>
+                            <p class="roledomain">
+                                {if isset($arrayOldReg[loopOldReg]['oldReg'])}
+                                    {$arrayOldReg[loopOldReg]['oldReg']->getTypeDescription()}
+                                {/if}
+                            </p>
+                            <p class="label">Motivation:</p>
+                            <p class="motivation">
+                                {if !empty($arrayOldReg[loopOldReg]['motivation'])}
+                                    {$arrayOldReg[loopOldReg]['motivation']->getText()}
+                                {/if}
+                            </p>
+                            <p class="label">Keywords:</p>
+                            <p class="keyword">
+                                {if isset($arrayOldReg[loopOldReg]['arrayKW'])}
+                                    {$arrayOldReg[loopOldReg]['arrayKW'][0]->getValue()}
+                                {/if}
+                            </p>
+                            <p class="keyword">
+                                {if isset($arrayOldReg[loopOldReg]['arrayKW'])}
+                                    {$arrayOldReg[loopOldReg]['arrayKW'][1]->getValue()}
+                                {/if}
+                            </p>
+                            <p class="keyword">
+                                {if isset($arrayOldReg[loopOldReg]['arrayKW'])}
+                                    {$arrayOldReg[loopOldReg]['arrayKW'][2]->getValue()}
+                                {/if}
+                            </p>
+                        </article>
+
+                    {/section}
+                </aside>
+            {/if}
+        {else}
+            <p class="error_msg" {$participant->getMessage()}</p>
+        {/if}
     {else}
-        <p class="error_msg" {$participant->getMessage()}</p>
+        <p class="error_msg">
+            You are not logged in, you cannot access this content.
+        </p>
     {/if}
-{else}
-    <p class="error_msg">
-        You are not logged in, you cannot access this content.
-    </p>
-{/if}
 </section>
--- user_inscriptions.php
+++ user_inscriptions.php
@@ -4,7 +4,7 @@
 /*
  * user_inscriptions.php
  *
- * Author : Andreane et Noemie
+ * Author : Christophe Rast
  * Date : 18.06.2013
  *
  * Description : user inscriptions, with old events
@@ -18,146 +18,98 @@ if ($tedx_manager->isLogged()) { //test if user is logged otherwise he displays 
             $tedx_manager->getLoggedPerson()->getContent()->getNo()
     );
     $smarty->assign('participant', $messageParticipant);
-    //Test the status of the registration
+    //---------------- Test the status of the registration ----------------------
     if ($messageParticipant->getStatus()) {
 
-        //gets the array of all the registrations of the current loggedin person
-        $arrayRegistrations = $tedx_manager->getRegistrationsByParticipant(
-                        $messageParticipant->getContent()
-                )->getContent();
-        if (!empty($arrayRegistrations)) { //test if there is registrations
-            $lengthArrayReg = count($arrayRegistrations);
-            // If the array is not empty, start processing them
-            $smarty->assign('registration', $arrayRegistrations[0]);
-            $person = $tedx_manager->getLoggedPerson()->getContent();
-            $participant = $messageParticipant->getContent();
-            $event = $tedx_manager->getEvent(
-                            $arrayRegistrations[0]->getEventNo()
-                    )->getContent();
-            //retrieve the status of current registration
-            $regStatus = $arrayRegistrations[0]->getStatus();
-            $smarty->assign('regStatus', $regStatus);
-            //assigns arguments for later queries
-            $argsPE = array(
-                'person' => $person,
+
+        /* ----------------------Registrations for Upcoming events------------------------------ */
+//prepare the upcoming event SQL request
+        $searchArgsUpcomingEvents = array(
+            'where' => "StartingDate >= '" . date('Y-m-d') . "'",
+            'orderBy' => 'StartingDate',
+            'orderByType' => 'ASC'
+        );
+//get the upcoming Event
+        $messsageUpcomingEvent = $tedx_manager->searchEvents($searchArgsUpcomingEvents);
+        //get back the list of all the events
+        $upcomingEvents = $messsageUpcomingEvent->getContent();
+        $arrayLastReg = array();
+        foreach ($upcomingEvents as $event) {
+            //gets the array of all the registrations of the current loggedin person
+            $argsLR = array(
+                'participant' => $messageParticipant->getContent(),
                 'event' => $event
             );
-            $argsPAE = array(
-                'participant' => $participant,
+            $msgLastReg = $tedx_manager->getLastRegistration($argsLR);
+            if ($msgLastReg->getStatus()) {
+                //gets motivation and keywords for the existant person and event
+                $motivation = $tedx_manager->getMotivationsByParticipantForEvent(
+                                $argsLR
+                        )->getContent();
+                $keywords = $tedx_manager->getKeywordsByPersonForEvent(
+                                array(
+                                    'person' => $tedx_manager->getLoggedPerson()->getContent(),
+                                    'event' => $event
+                        ))->getContent();
+                //Creates a table for the event and participant
+                //-array( --------------------------------------------------------------
+                // array ( aRegistration, aMotivation, keywords) -----------------------
+                // array ( aRegistration, aMotivation, keywords) -----------------------
+                // etc...)
+                $arrayLastReg[] = array(
+                    'registration' => $msgLastReg->getContent(),
+                    'motivation' => $motivation[0],
+                    'arrayKW' => $keywords,
+                    'event' => $event);
+            }
+        }
+        $smarty->assign('arrayReg', $arrayLastReg);
+        /* ----------------------Registrations for past events------------------------------ */
+
+        //prepare the past event SQL request
+        $searchArgsPastEvents = array(
+            'where' => "EndingDate >= '" . date('Y-m-d') . "'",
+            'orderBy' => 'EndingDate',
+            'orderByType' => 'DESC'
+        );
+        //get the upcoming Event
+        $messsagePastEvent = $tedx_manager->searchEvents($searchArgsUpcomingEvents);
+        //get back the list of all the events
+        $pastEvents = $messsagePastEvent->getContent();
+        $arrayPastReg = array();
+        foreach ($pastEvents as $event) {
+            //gets the array of all the registrations of the current loggedin person
+            $argsPR = array(
+                'participant' => $messageParticipant->getContent(),
                 'event' => $event
             );
-            $arrayKeywords = $tedx_manager->getKeywordsByPersonForEvent($argsPE)->getContent();
-            if ($regStatus == 'Sent' || $regStatus == 'Accepted' || $regStatus == 'Rejected') {
-// --------------- registration is locked if status doesn't match --------------
-                //gets the DB content and displays it
-                //assigns to smarty the text of the motivation defined by the event and participant
-                $frozenMot = $tedx_manager->getMotivationsByParticipantForEvent(
-                                $argsPAE
-                        )->getContent()->getText();
-                $smarty->assign('getMotivation', $frozenMot);
-                //assigns to smarty the text of each keyword of the person
-                for ($n = 0; $n < 3; $n++) {//cylce through keywords to assign them to smarty
-                    $smarty->assign('getKW' . ($n + 1), $arrayKeywords[$n]->getValue());
-                }
-            } else {
-// ------------------ registration is open for modifications -------------------
-                //the content is saved but not submitted yet and can be modified
-                $kw = array();
-                $saveKW = FALSE; //defines is the keywords were modified and must be saved
-                for ($n = 0; $n < 3; $n++) {//cycle through the 3 keywords and modifies them if necessary
-                    if (empty($_POST['Keyword' . ($n + 1)])) {
-                        //if the keyword is not posted it is retrieved from the DB
-                        //and displayed on the page
-                        //retrieve the array of keywords
-                        if (!empty($arrayKeywords[$n])) {//gets the nth KW
-                            $kw[$n] = $arrayKeywords[$n]->getValue();
-                            $smarty->assign('kw' . ($n + 1), $kw[$n]);
-                        } else {
-                            $kw[$n] = null;
-                            $smarty->assign('kw' . ($n + 1), 'No keyword to display');
-                        }
-                        $saveKW = FALSE;
-                    } else {//if the keyword is posted, the old one is archived
-                        if (!empty($arrayKeywords[$n])) {
-                            $argsKW = array(//create array from the retrieved KW 
-                                'value' => $arrayKeywords[$n]->getValue(),
-                                'person' => $person,
-                                'event' => $event
-                            );
-                            //archive the previous KW
-                            $msgArchiveKW = $tedx_manager->archiveKeyword($argsKW);
-                        }
-                        $kw[$n] = $_POST['Keyword' . ($n + 1)]; // gets the posted KW and store it
-                        $smarty->assign('kw' . ($n + 1), $kw[$n]);
-                        $saveKW = TRUE;
-                    }
-                }//end FOR 3 keywords
-                if ($saveKW) { //stores the new keywords
-                    $args = array(//create the argument array
-                        'listOfValues' => $kw,
-                        'person' => $person,
-                        'event' => $event
-                    );
-                    //add the new KW in place of the previous one
-                    $msgAddKW = $tedx_manager->addKeywordsToAnEvent($args);
-//                    if ($msgAddKW->getStatus()) {
-//                        //changeregistrationStatus() to submitted
-//                    } else {
-//                        print ('<p class="error_msg">');
-//                        print ("Error during the keyword process:");
-//                        print $msgAddKW->getMessage();
-//                        print ("</p>");
-//                    }
-                }
-                //--------------Gets or send the motivation of the participant----------
-                $arrayOldMotivation = $tedx_manager->getMotivationsByParticipantForEvent($argsPAE)->getContent();
-                if (isset($arrayOldMotivation)) {
-                    $oldMotivation = $arrayOldMotivation[0]->getText();
-                }
-                if (empty($_POST['motivation'])) {
-                    // retrieve the motivation
-                    if (!empty($arrayOldMotivation)) {//if the motivation is filled, it is set
-                        $smarty->assign('motivation', $oldMotivation);
-                    } else {//if not, 
-                        $smarty->assign('motivation', 'Please fill in you motivation');
-                    }
-                } else {
-                    if (!empty($arrayOldMotivation)) {
-                        $argsOMPE = array(
-                            'text' => $oldMotivation,
-                            'participant' => $participant,
-                            'event' => $event
-                        );
-                        echo $tedx_manager->archiveMotivationToAnEvent($argsOMPE)->getMessage();
-                    }
-                    $motiv = $_POST['motivation'];
-                    $argsMEP = array(
-                        'text' => $motiv,
-                        'event' => $event,
-                        'participant' => $participant);
-                    var_dump($argsMEP);
-                    $msgAddMotiv = $tedx_manager->addMotivationToAnEvent($argsMEP);
-                    print ('<p class="error_msg">');
-                    print $msgAddMotiv->getMessage();
-                    print ("</p>");
-                }
-            }// end IF status (open, locked)
-            //
-// ---------Gets events and last events for the historic -----------------------
-            // declare the request arguments for events
-            $searchArgs = array();
-            //get back the result of the events request
-            $messageSearchEvents = $tedx_manager->searchEvents($searchArgs);
-            //if the message is not an error message
-            if ($messageSearchEvents->getStatus()) {
-                //get back the list of all the events
-                $events = $messageSearchEvents->getContent();
-                //stock the events list in smarty
-                $smarty->assign('eventsObjects', $events);
-            }//if
-            $smarty->display('user_inscriptions.tpl');
-        }//end IF registrations are here
+            $msgPastReg = $tedx_manager->getRegistrationHistory($argsLR);
+            if ($msgPastReg->getStatus()) {
+                //gets motivation and keywords for the existant person and event
+                $motivation = $tedx_manager->getMotivationsByParticipantForEvent(
+                                $argsPR
+                        )->getContent();
+                $keywords = $tedx_manager->getKeywordsByPersonForEvent(
+                                array(
+                                    'person' => $tedx_manager->getLoggedPerson()->getContent(),
+                                    'event' => $event
+                        ))->getContent();
+                //Creates a table for the event and participant
+                //-array( --------------------------------------------------------------
+                // array ( aRegistration, aMotivation, keywords) -----------------------
+                // array ( aRegistration, aMotivation, keywords) -----------------------
+                // etc...)
+                $msgPastReg = $msgPastReg->getContent();
+                $arrayPastReg[] = array(
+                    'oldReg' => $msgPastReg[0],
+                    'motivation' => $motivation[0],
+                    'arrayKW' => $keywords,
+                    'event' => $event);
+            }
+        }
+        $smarty->assign('arrayOldReg', $arrayPastReg);
     }
 }
+$smarty->display('user_inscriptions.tpl');
 include 'userbar.php';
 ?>
