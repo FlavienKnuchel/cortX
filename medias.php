c30ce3f5acfdf3ff5af23404d2ecba57e590a6cd
flavien knuchel <knuchelf@gmail.com>
2013-06-25 10:53:18 +0200

    mise en place de la page media

--- medias.php
+++ medias.php
@@ -11,6 +11,76 @@
 
 include 'header.php';
 include 'menu_frontend.php';
+
+//get all the talks
+$messageTalks=$tedx_manager->getTalks();
+//check if the request went well
+if($messageTalks->getStatus()){
+    //get the content of the message
+   $talks=$messageTalks->getContent();
+    //create the videos reference array to fill
+    $videosRef=array();
+    //create the title array to fill
+    $talkTitles=array();
+    //create the descriptions array to fill
+    $talkDescription=array();
+    $talksArray=array();
+    //for each talk
+    foreach($talks as $talk){
+        //get the url
+        $url=$talk->getVideoUrl();
+        //get the reference
+        $talkRef=getYoutubeRef($url);
+        //create an array with all the infos of the talk
+        $combination=array(
+            'video'=>createVideoUrl($talkRef),
+            'img'=>createImgUrl($talkRef),
+            'title'=>$talk->getVideoTitle(),
+            'eventNo'=>$talk->getEventNo(),
+            'speakerNo'=>$talk->getSpeakerPersonNo()
+        );
+        //put it in the
+        array_push($talksArray,$combination);
+    }//foreach
+
+    //assign the new combined array to smarty
+    $smarty->assign('talksArray',$talksArray);
+}//if
+else{
+    $error="No video found";
+}//else
+
 $smarty->display('media.tpl');
 include 'userbar.php';
+
+/*------------------------------ functions ------------------------------*/
+//used to create the youtube image thumbnail url from refs
+function createImgUrl($ref){
+    //create the good image url
+    $fullImgRef="http://img.youtube.com/vi/".$ref."/mqdefault.jpg";
+    //return the image url
+    return $fullImgRef;
+}
+
+//create a youtube embed  url from the ref
+function createVideoUrl($ref){
+    //create the good video url
+    $fullVideoRef="http://www.youtube.com/embed/".$ref."?autoplay=1&fs=1&rel=0&enablejsapi=1&playerapiid=ytplayer";
+    //return the array
+    return $fullVideoRef;
+}
+
+//recovers the youtube video reference from a random yoyutube url
+function getYoutubeRef($url){
+    //break it at the = sign
+    $url=explode("=",$url);
+    //select the last part with the ref
+    $ref=$url[1];
+    //break it at the & sign (to take away the &lists)
+    $ref=explode("&",$ref);
+    //select the first part (clean ref)
+    $finalRef=$ref[0];
+    //push it into the array
+    return $finalRef;
+}
 ?>
--- templates/media.tpl
+++ templates/media.tpl
@@ -7,42 +7,19 @@
  * Description : structure of the video wall
  */  -->
 
-<div id="videos_media">        
-    <h1>TEDxLausanne 2013</h1>
-    <div class="row">
-        <div class="offset2 span8" >
-            <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
+<section class="row">
+    <div class="offset2 span8">
+    {section  loop=$talksArray name=talk}
+       {if $smarty.section.talk.first}
+           <h1>Videos from TEDxLausanne</h1>
+       {/if}
+        <p>
+            <h3>{$talksArray[talk].title}</h3>
+            <a href="{$talksArray[talk].video}">
+                <img src="{$talksArray[talk].img}" alt="{$talksArray[talk].title}"/>
             </a>
-            <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>
-            <a href="#" class=" ">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>
-            <a href="#" class="">
-                <img src="img/videos/video1.jpg" alt="Miniature video"/>
-            </a>    
-        </div>
+            <p></p><a href="video.php?eventNo={$talksArray[talk].eventNo}&speakerNo={$talksArray[talk].speakerNo}">more...</a></
+        </p>
+        {/section}
     </div>
-
-    <div class="row">
-        <h1>TEDxLausanne 2012</h1>
-        <div class="row">
-            <div class="offset2 span8">
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>
-                <a href="#" class="">
-                    <img src="img/videos/video1.jpg" alt="Miniature video"/>
-                </a>   
-            </div>
-        </div>
-    </div>
-</div>
+</section>
