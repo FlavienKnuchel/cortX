c48978b93bc2dc4d77d74153f6a7b477651f811b
flavien knuchel <knuchelf@gmail.com>
2013-06-27 10:00:03 +0200

    page login.php et tpl

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if ($tedx_manager->isLogged()) {
     if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'logout') {
         header('Location: home.php');
     } else {
-        if (isset($_POST['username']) && isset($_POST['password'])) {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
             $message = $tedx_manager->login($_POST['username'], $_POST['password']); //login test
             if ($tedx_manager->isLogged()) {
                 if ($tedx_manager->isOrganizer() || $tedx_manager->isValidator() || $tedx_manager->isAdministrator() || $tedx_manager->isSuperadmin()) {//redirect the user according to his rights
@@ -37,12 +37,14 @@ if ($tedx_manager->isLogged()) {
                 }
             } else {
                 include 'header.php';
+                include 'menu_frontend.php';
                 $smarty->assign('errorlogin', $message->getMessage()); //send the login error message
                 $smarty->display('login.tpl');
                 include 'userbar.php';
             }
         } else {
             include 'header.php';
+            include 'menu_frontend.php';
             $smarty->assign('errorlogin', 'You did not fill all the fields.');
             $smarty->display('login.tpl');
             include 'userbar.php';
--- templates/login.tpl
+++ templates/login.tpl
@@ -8,11 +8,23 @@
  *
  *
 -->
-<aside id="loginToAccount">
-    <p class="error_msg">{$errorlogin}</p>
+{if isset($errorlogin)}
+    <section class="row firstElement">
+        <article class="offset2 span8 badMessage ">
+            <p>{$errorlogin}</p>
+        </article>
+    </section>
+{/if}
+<div id="loginForm" class="offset5 span2 lastElement" id="passwordForm">
     <form method="POST" action="login.php">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="Login" value="OK"/>
+        <div class="span12">
+        <label class="span12" for="username" title="username">Username:</label>
+        <input class="span12" type="text" name="username" placeholder="Username">
+        </div>
+        <div class="span12">
+        <label class="span12" for="password" title="password">Password:</label>
+        <input class="span12" type="password" name="password" placeholder="Password">
+        </div>
+        <input class="span12" type="submit" name="Login" value="OK"/>
     </form>
-</aside>
+</div>
