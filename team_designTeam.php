a708c8ea6c878e7a02be232a20f165427ec6028d
knuchel Flavien <knuchelf@gmail.com>
2013-06-27 22:53:54 +0200

    debug majuscules

---
+++ team_designTeam.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * team_designTeam.php
+ *
+ * Author : Anthony Da Silva
+ * Date : 26.06.2013
+ *
+ * Description :
+ */
+
+include 'header.php';
+include 'menu_frontend.php';
+$smarty->display('team_designTeam.tpl');
+include 'userbar.php';
+?>
